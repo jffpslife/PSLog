009cfdacc1791af4f167c85679c3509d07d33217
Soomin Kim <jffpslife@gmail.com>
2022-03-13 13:54:10 +0900

    Do not print debugging messages

--- utils/test.sh
+++ utils/test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 FILECOUNT=$(find $THISDIR/$MYDIRNAME/tests -type f | wc -l)
-echo $FILECOUNT
 LASTIDX=$((FILECOUNT - 1))
-echo $LASTIDX
 
 for (( i=0; i<=$LASTIDX; i++)); do
 	echo "Test $i"
