a85c35d0d3ffd4c133523efaae0dc75ec75a8b78
Soomin Kim <jffpslife@gmail.com>
2022-03-13 16:08:07 +0900

    Solve bj/11651

---
+++ bj/11651/11651.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Point {
+  int X;
+  int Y;
+};
+
+int N;
+vector<Point> P;
+
+bool compare(struct Point p1, struct Point p2) {
+  if (p1.Y == p2.Y) {
+    return p1.X < p2.X;
+  } else {
+    return p1.Y < p2.Y;
+  }
+}
+
+void solve() {
+  sort(P.begin() + 1, P.end(), compare);
+}
+
+void print(int i) {
+  Point p = P[i];
+  printf("%d %d\n", p.X, p.Y);
+}
+
+int main() {
+  scanf("%d", &N);
+  P.resize(N + 1);
+  for (int i = 1; i <= N; i++) {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    P[i].X = x;
+    P[i].Y = y;
+  }
+
+  solve();
+
+  for (int i = 1; i <= N; i++) {
+    print(i);
+  }
+
+  return 0;
+}
