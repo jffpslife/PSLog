568d4feda369d5c5fb8fe0fe2a3229cc5df373e3
Soomin Kim <jffpslife@gmail.com>
2022-03-13 19:47:24 +0900

    Solve bj/15651

---
+++ bj/15651/15651.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+int history[7];
+
+void loop(int step, int cur) {
+  history[step] = cur;
+  step++;
+
+  if (step == M) {
+    for (int i = 0; i < M; i++) {
+      printf("%d ", history[i]);
+    }
+    printf("\n");
+  } else {
+    for (int i = 1; i <= N; i++) {
+      loop(step, i);
+    }
+  }
+}
+
+void solve() {
+  for (int i = 1; i <= N; i++) {
+    loop(0, i);
+  }
+}
+
+int main() {
+  scanf("%d %d", &N, &M);
+
+  solve();
+
+  return 0;
+}
