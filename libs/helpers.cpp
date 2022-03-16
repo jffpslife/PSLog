b005c2fdd023598f7990661ce122bce9c7a8602c
Soomin Kim <jffpslife@gmail.com>
2022-03-16 14:41:53 +0900

    Add LCS to library

--- libs/helpers.cpp
+++ libs/helpers.cpp
@@ -39,3 +39,35 @@ void merge_sort(int left, int right) {
     merge(left, mid, right);
   }
 }
+
+// LCS
+int DP[L1][L2];
+int B[L1][L2];
+
+int dp(char *S1, char *S2, int i, int j) {
+  if (B[i][j]) {
+    return DP[i][j];
+  } else {
+    if (i < 0 || j < 0) {
+      return 0;
+    } else {
+      if (S1[i] == S2[j]) {
+        int res = dp(i - 1, j - 1) + 1;
+        B[i][j] = true;
+        DP[i][j] = res;
+        return res;
+      } else {
+        int res1 = dp(i - 1, j);
+        int res2 = dp(i, j - 1);
+        int res = max(res1, res2);
+        B[i][j] = true;
+        DP[i][j] = res;
+        return res;
+      }
+    }
+  }
+}
+
+void lcs(char *S1, char *S2, int L1, int L2) {
+  return dp(S1, S2, L1 - 1, L2 - 1);
+}
