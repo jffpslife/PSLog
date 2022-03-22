9f558124b099019cafd102b6f1695ac5b129af07
Soomin Kim <jffpslife@gmail.com>
2022-03-22 22:58:59 +0900

    Add a template for codeforces

---
+++ templates/cf.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int T;
+
+int main() {
+  setbuf(stdout, NULL);
+
+  scanf("%d", &T);
+  for (int t = 0; t < T; t++) {
+
+  }
+
+  return 0;
+}
--- utils/init.sh
+++ utils/init.sh
@@ -3,11 +3,16 @@
 set -e
 
 # Currently supported platforms
-PLATFORMS=("bj")
+PLATFORMS=("bj", "cf")
 
 if [[ "bj" == $MYPLATFORM ]]; then
   mkdir -p $MYDIRNAME
   cp $THISDIR/templates/bj.cpp $MYDIRNAME/$MYNAME.cpp
+
+  mkdir -p $THISDIR/cur/tests
+elif [[ "cf" == $MYPLATFORM ]]; then
+  mkdir -p $MYDIRNAME
+  cp $THISDIR/templates/cf.cpp $MYDIRNAME/$MYNAME.cpp
 
   mkdir -p $THISDIR/cur/tests
 else
